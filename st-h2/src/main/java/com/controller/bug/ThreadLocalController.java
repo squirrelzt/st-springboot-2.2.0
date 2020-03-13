53aee4a7f2bd02e427aaba1dc7dda954cce45f67
squirrelzt <squirrelzt@126.com>
2020-03-13 20:33:38 +0800

    threadlocal存储用户信息混乱

---
+++ st-h2/src/main/java/com/controller/bug/ThreadLocalController.java
@@ -0,0 +1,47 @@
+package com.controller.bug;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("bug/threadlocal")
+public class ThreadLocalController {
+    private ThreadLocal<Integer> currentUser = ThreadLocal.withInitial(() -> null);
+
+
+    @RequestMapping("wrong")
+    public Map wrong(@RequestParam("userId") Integer userId) {
+        //设置用户信息之前先查询一次ThreadLocal中的用户信息
+        String before  = Thread.currentThread().getName() + ":" + currentUser.get();
+        //设置用户信息到ThreadLocal
+        currentUser.set(userId);
+        //设置用户信息之后再查询一次ThreadLocal中的用户信息
+        String after  = Thread.currentThread().getName() + ":" + currentUser.get();
+        //汇总输出两次查询结果
+        Map result = new HashMap();
+        result.put("before", before);
+        result.put("after", after);
+        return result;
+    }
+
+
+    @GetMapping("right")
+    public Map right(@RequestParam("userId") Integer userId) {
+        String before  = Thread.currentThread().getName() + ":" + currentUser.get();
+        currentUser.set(userId);
+        try {
+            String after = Thread.currentThread().getName() + ":" + currentUser.get();
+            Map result = new HashMap();
+            result.put("before", before);
+            result.put("after", after);
+            return result;
+        } finally {
+            //在finally代码块中删除ThreadLocal中的数据，确保数据不串
+            currentUser.remove();
+        }
+    }
+}
