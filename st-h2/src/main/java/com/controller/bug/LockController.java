864fa5fa06e939d53168ae717bf56da5f648946d
squirrelzt <squirrelzt@126.com>
2020-03-15 10:57:19 +0800

    synchronized锁粒度

--- st-h2/src/main/java/com/controller/bug/LockController.java
+++ st-h2/src/main/java/com/controller/bug/LockController.java
@@ -1,12 +1,17 @@
 package com.controller.bug;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
+@Slf4j
 @RestController
 @RequestMapping("bug/lock")
 public class LockController {
@@ -26,5 +31,46 @@ public class LockController {
         //多线程循环一定次数调用Data类不同实例的wrong方法
         IntStream.rangeClosed(1, count).parallel().forEach(i -> new LockDataDomain().right());
         return LockDataDomain.getCounter();
+    }
+
+
+    private List<Integer> data = new ArrayList<>();
+
+    //不涉及共享资源的慢方法
+    private void slow() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(10);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    //错误的加锁方法
+    @GetMapping("slow/wrong")
+    public int wrong() {
+        long begin = System.currentTimeMillis();
+        IntStream.rangeClosed(1, 1000).parallel().forEach(i -> {
+            //加锁粒度太粗了
+            synchronized (this) {
+                slow();
+                data.add(i);
+            }
+        });
+        log.info("took:{}", System.currentTimeMillis() - begin);
+        return data.size();
+    }
+
+    //正确的加锁方法
+    @GetMapping("slow/right")
+    public int right() {
+        long begin = System.currentTimeMillis();
+        IntStream.rangeClosed(1, 1000).parallel().forEach(i -> {
+            slow();
+            //只对List加锁
+            synchronized (data) {
+                data.add(i);
+            }
+        });
+        log.info("took:{}", System.currentTimeMillis() - begin);
+        return data.size();
     }
 }
